9cabbee012df2d7d9ba0688993e74f015f2830e9
Rushi <189489rishi@gmail.com>
2022-03-09 17:24:34 +0530

    recursive Iteration

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿//  Datastrecture Algorithm = Prime Number range (0 - 1000)
+﻿//  Datastrecture Algorithm recursive 
 
 using System;
 
@@ -8,8 +8,65 @@ namespace Algorithm_Problems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prime Numbers program");
-            PrimeNumbers.NumberRange(0,1000);
+            String word = Console.ReadLine();
+            int lengthOfWord = word.Length;
+            RecursivePermutation(word, 0, lengthOfWord - 1);
+            IterativePermutation(word);
+        }
+        public static void RecursivePermutation(string word, int a, int b)
+        {
+            if (a == b)
+            {
+                Console.WriteLine(word);
+            }
+            else
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    word = Swapping(word, a, i);
+                    RecursivePermutation(word, a + 1, b);
+                    word = Swapping(word, a, i);
+                }
+            }
+        }
+        public static string Swapping(string word, int i, int j)
+        {
+            char temp;
+            char[] charactersOfWord = word.ToCharArray();
+            temp = charactersOfWord[i];
+            charactersOfWord[i] = charactersOfWord[j];
+            charactersOfWord[j] = temp;
+            string str = new string(charactersOfWord);
+            return str;
+        }
+        public static int CalculatingFactorial(int lengthOfWord)
+        {
+            int factorial = 1;
+            for (int i = 1; i <= lengthOfWord; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+        public static void IterativePermutation(string word)
+        {
+            int lengthOfWord = word.Length;
+            int factorial = CalculatingFactorial(lengthOfWord);
+            for (int i = 0; i < factorial; i++)
+            {
+                string originalWord = word;
+                int temp = i;
+                for (int j = word.Length; j >= 1; j--)
+                {
+                    int quotient = temp / j;
+                    int reminder = temp % j;
+                    Console.Write(originalWord[reminder]);
+                    originalWord = originalWord.Remove(reminder, 1);
+                    temp = quotient;
+                }
+                Console.WriteLine();
+            }
         }
     }
+    
 }

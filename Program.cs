ad050af6cc7167ae5a80fc910f69a959d4bfb23c
Rushi <189489rishi@gmail.com>
2022-03-09 18:45:36 +0530

    creating palindrome

--- Program.cs
+++ Program.cs
@@ -1,81 +1,27 @@
 ﻿using System;
 
-namespace BinarySearchTree
+namespace PalindromeCheck
 {
     class Program
     {
-        public class Node
+        static void Main(string[] args)
         {
-            public int key;
-            public Node left, right;
-
-            public Node(int item)
+            string s, revs = "";
+            Console.WriteLine(" Enter string");
+            s = Console.ReadLine();
+            for (int i = s.Length - 1; i >= 0; i--) //String Reverse  
             {
-                key = item;
-                left = right = null;
+                revs += s[i].ToString();
             }
-        }
-        // Root of BST
-        Node root;
-
-        // Constructor
-        Program()
-        {
-            root = null;
-        }
-        // This method mainly calls insertRec()
-        void insert(int key)
-        {
-            root = insertRec(root, key);
-        }
-        // a new key in BST
-        Node insertRec(Node root, int key)
-        {
-
-            // If the tree is empty,
-            // return a new node
-            if (root == null)
+            if (revs == s) // Checking whether string is palindrome or not  
             {
-                root = new Node(key);
-                return root;
+                Console.WriteLine("String is Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
             }
-            // Otherwise, recur down the tree
-            if (key < root.key)
-                root.left = insertRec(root.left, key);
-            else if (key > root.key)
-                root.right = insertRec(root.right, key);
-
-            // Return the (unchanged) node pointer
-            return root;
-        }
-        // This method mainly calls InorderRec()
-        void inorder()
-        {
-            inorderRec(root);
-        }
-        // do inorder traversal of BST
-        void inorderRec(Node root)
-        {
-            if (root != null)
+            else
             {
-                inorderRec(root.left);
-                Console.WriteLine(root.key);
-                inorderRec(root.right);
+                Console.WriteLine("String is not Palindrome \n Entered String Was {0} and reverse string is {1}", s, revs);
             }
-        }
-        public static void Main(String[] args)
-        {
-            Program tree = new Program();
-            tree.insert(50);
-            tree.insert(30);
-            tree.insert(20);
-            tree.insert(40);
-            tree.insert(70);
-            tree.insert(60);
-            tree.insert(80);
-
-            // Print inorder traversal of the BST
-            tree.inorder();
+            Console.ReadKey();
         }
     }
 }

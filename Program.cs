03dd9819cad15bfe05e5f68bdb3b719bf94371d2
Rushi <189489rishi@gmail.com>
2022-03-09 17:09:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//  Datastrecture Algorithm = Prime Number range (0 - 1000)
+
+using System;
+
+namespace Algorithm_Problems
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prime Numbers program");
+            PrimeNumbers.NumberRange(0,1000);
+        }
+    }
+}
